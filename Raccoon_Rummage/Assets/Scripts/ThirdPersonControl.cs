097e3ebc0c984c1a0a3212856724fafe178dacdc
CptVipezzz <127725281+CptVipezzz@users.noreply.github.com>
2023-04-01 16:42:58 +0800

    bug fixed collisions working

--- Raccoon_Rummage/Assets/Scripts/ThirdPersonControl.cs
+++ Raccoon_Rummage/Assets/Scripts/ThirdPersonControl.cs
@@ -14,7 +14,6 @@ public class ThirdPersonControl : MonoBehaviour
     public Transform cam;
 
     public float speed = 6f;
-
     public float turnSmooth = 0.1f;
 
     float turnSmoothVelocity;
@@ -22,18 +21,21 @@ public class ThirdPersonControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Gets the horizontal and vertical input values and puts them into a Vec3
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if (direction.magnitude >= 1f);
+        //Checks for an input 
+        if (direction.magnitude >= 1f)
         {
-            //UnityEngine.Debug.Log(direction.magnitude);
 
+            //Gets the current angle and then rotates the gameobject to the new angle based on the inputs
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; 
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmooth);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
+            //Moves the gameobject forward
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }        
