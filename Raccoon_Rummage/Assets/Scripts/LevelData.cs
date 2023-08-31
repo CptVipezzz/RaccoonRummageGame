2b352603ac57f289ddbc7f5e546840d8c812d233
CptVipezzz <127725281+CptVipezzz@users.noreply.github.com>
2023-08-31 20:24:40 +0800

    minor adjustments

--- Raccoon_Rummage/Assets/Scripts/LevelData.cs
+++ Raccoon_Rummage/Assets/Scripts/LevelData.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class LevelData : MonoBehaviour
 {
+    [SerializeField] int levelTime;
+    [SerializeField] int levelScore;
+    [SerializeField] string levelName;
+
     // Start is called before the first frame update
     void Start()
     {
