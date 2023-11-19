db6b6206b25abee63b5598119eb60166d11b8957
CptVipezzz <127725281+CptVipezzz@users.noreply.github.com>
2023-11-19 17:16:24 +0800

    Pigeons added

---
+++ Raccoon_Rummage/Assets/Scripts/AnimPigeon.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimPigeon : MonoBehaviour
+{
+    Animator anim;
+
+    public bool detected = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        detected = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (detected == false)
+        {
+            anim.SetBool("Scared", false);
+            
+        }
+        else if (detected == true)
+        {
+            anim.SetBool("Scared", true);
+            StartCoroutine(CullModel());
+        }
+    }
+
+    IEnumerator CullModel()
+    {
+        yield return new WaitForSeconds(1.5f);
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Raccoon_Rummage/Assets/Scripts/SingleDetecterController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleDetecterController : MonoBehaviour
+{
+    [SerializeField] private GameObject detecter;
+    [SerializeField] private GameObject[] pigeons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            detecter.SetActive(false);
+            for(int i = 0; i < pigeons.Length; i++)
+            {
+                AnimPigeon tmp;
+                tmp = pigeons[i].GetComponent<AnimPigeon>();
+                tmp.detected = true;
+            }
+        }
+    }
+}
